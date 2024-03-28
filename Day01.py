9c36b78f705178fbd7da1e7f6d7eccbfdf97a875
Sirancheevi29 <nssjeevi@gmail.com>
2024-03-28 17:41:30 +0100

    Day01

---
+++ Day01.py
@@ -0,0 +1,30 @@
+# Print Hello world
+print("Hello world!")
+
+# string escape characters
+print("This is samples text")
+
+print('This is "simple" text')
+
+print("This is \"simple\" text")
+
+#string concendination
+print("Hello "+"world")
+
+#Input function
+print("Hello "+ input("what is your name"))
+
+#Input +/-
+FirstName =input()
+LastName=input()
+print("Hello "+FirstName + " "+ LastName)
+
+#Brand name generator
+#1.Welcome greetings
+print("Welcome to brand name generator\n")
+#2.Get First Name
+FirstName1=input("Please enter your first name?\n")
+#3.Enter your city
+City=input("Please enter your city?\n")
+#4. brand name
+print("Hello your brand name is: "+FirstName1+" "+ City)
